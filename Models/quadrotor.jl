8af8d2733e74cbde08bda045eb83fd673ff8ed87
rbonalli <riccardo.bonalli@gmail.com>
2019-12-16 09:36:45 +0100

    Comments added to quadrotor.jl

--- Models/quadrotor.jl
+++ Models/quadrotor.jl
@@ -1,32 +1,40 @@
+# ------------------------------------------------------------------
+# -   Model for the quadrotor example - Riccardo Bonalli 12/2019   -
+# ------------------------------------------------------------------
+
+
+
 export Quadrotor
 
+
+
+# Model quadrotor as a Julia class
+
 mutable struct Quadrotor
-    # State: r, v
+    # State (r, v) and control (u, Γ) dimensions
     x_dim
-    # Control: u, Γ
     u_dim
 
-    # Dynamics
+    # Dynamics and linearized dynamics
     f
     A
     B
 
     # Model constants
     gravity
-    uMin
-    uMax
     θMax
 
-    # Problem  
+    # Problem settings
     x_init
     x_final
-    tf_guess
+    tf
     xMin
     xMax
+    uMin
+    uMax
 
-    # Sphere obstacles [(x,y),r]
+    # Cylindrical obstacles (modeled by a center (x,y) and a radius r) and polygon obstacles (not used in this example)
     obstacles
-    # Polygons
     poly_obstacles
 
     # GuSTO parameters
@@ -42,25 +50,26 @@ mutable struct Quadrotor
     convergence_threshold
 end
 
+
+
+# The problem is set in the class constructor
+
 function Quadrotor()
     x_dim = 6
     u_dim = 4
 
-    # Model constants
     gravity = [0;0;9.81]
-    uMin = 0.6
-    uMax = 23.2
     θMax = pi/3.0
 
-    # Problem
     x_init  = [0;0;0 ; 0;0;0]
     x_final = [2.5;6.;0 ; 0;0;0]
-    tf_guess = 2.#1.99 # s
-    myInf = 1.0e6
+    tf = 2.
+    myInf = 1.0e6 # Adopted to detect initial and final condition-free state variables
     xMin = [-0.1;-0.1;-myInf ; -myInf;-myInf;-myInf]
     xMax = [4;7;myInf ; myInf;myInf;myInf]
+    uMin = 0.6
+    uMax = 23.2
 
-    # GuSTO parameters
     Delta0 = 100.
     omega0 = 1.
     omegamax = 1.0e9
@@ -73,20 +82,20 @@ function Quadrotor()
     convergence_threshold = 5.0
 
 
-    # Cylindrical obstacles [(x,y),r (= datum given in the CSM paper)]
+    # Cylindrical obstacles in the form [(x,y),r]
     obstacles = []
     obs = [[1.0,2.0],1.0/2.5]
     push!(obstacles, obs)
     obs = [[2.0,5.0],1.0/2.5]
     push!(obstacles, obs)
 
-    # Polygonal obstacles
+    # Polygonal obstacles are not used in this example
     poly_obstacles = []
 
     Quadrotor(x_dim, u_dim,
              [], [], [],
-             gravity, uMin, uMax, θMax,
-             x_init, x_final, tf_guess, xMin, xMax,
+             gravity, θMax,
+             x_init, x_final, tf, xMin, xMax, uMin, uMax,
              obstacles,
              poly_obstacles,
              Delta0,
@@ -101,9 +110,17 @@ function Quadrotor()
              convergence_threshold)
 end
 
+
+
+# Method that returns the GuSTO parameters (used for set up)
+
 function get_initial_gusto_parameters(m::Quadrotor)
     return m.Delta0, m.omega0, m.omegamax, m.epsilon, m.rho0, m.rho1, m.beta_succ, m.beta_fail, m.gamma_fail, m.convergence_threshold
 end
+
+
+
+# GuSTO is intialized by zero controls and velocities, and a straight-line in position
 
 function initialize_trajectory(model::Quadrotor, N::Int)
   x_dim,  u_dim   = model.x_dim, model.u_dim
@@ -115,12 +132,17 @@ function initialize_trajectory(model::Quadrotor, N::Int)
   x1 = x_final[1:dimR]
   X[1:dimR,:] = hcat(range(x0, stop=x1, length=N)...)
   for i = 1:N
-    X[dimR+1:2*dimR,i] = zeros(dimR)#(x1-x0)/norm(x1-x0)
+    X[dimR+1:2*dimR,i] = zeros(dimR)
   end
   U = zeros(u_dim, N-1)
 
   return X, U
 end
+
+
+
+# Method that returns the convergence ratio ρ (in percentage) between iterations
+# The quantities X, U denote the actual solution over time, whereas Xp, Up denote the solution at the previous step over time
 
 function convergence_metric(model::Quadrotor, X, U, Xp, Up)
     x_dim = model.x_dim
@@ -136,62 +158,49 @@ function convergence_metric(model::Quadrotor, X, U, Xp, Up)
         max_den = val > max_den ? val : max_den
     end
 
-    # Percentage error
+    # Returning percentage error
     return max_num*100.0/max_den
 end
 
 
 
-# --------------------------------------------
-# -               OBJECTIVE                  -
-# --------------------------------------------
+# Method that returns the original cost
 
 function true_cost(model::Quadrotor, X, U, Xp, Up)
     x_dim, u_dim = model.x_dim, model.u_dim
     cost = 0.
 
-    # ∫ Γ(t)^2 dt
     for k = 1:length(U[1,:])
-        cost += U[u_dim,k]^2
+        cost += U[u_dim,k]^2 # This corresponds to ∫ Γ(t)^2 dt
     end
 
     return cost
 end
 
-# --------------------------------------------
+
+
+# The following methods return the i-th coordinate at the k-th iteration of the various constraints and their linearized versions (when needed)
+# These are returned in the form " g(t,x(t),u(t)) <= 0 "
 
 
 
-# --------------------------------------------
-# -               CONSTRAINTS                -
-# --------------------------------------------
-
-# Linear
-# function control_linear_constraints(model::Quadrotor, X, U, Xp, Up, k, i)
-#     x_dim, u_dim = model.x_dim, model.u_dim
-#     uMin, uMax, θMax = model.uMin, model.uMax, model.θMax
-
-#     if i == 1
-#       return uMin - U[u_dim,k]
-#     elseif i == 2
-#       return U[u_dim,k] - uMax
-#     elseif i == 3
-#       return U[u_dim,k]*cos(θMax) - U[u_dim-1,k]
-#     else
-#       println("ERROR - TOO MANY LINEAR CONTROL CONSTRAINTS")
-#     end
-# end
+# Method that gathers all the lienar control constraints
 
 function control_linear_constraints(model::Quadrotor, X, U, Xp, Up, k, i)
     x_dim, u_dim = model.x_dim, model.u_dim
     uMin, uMax, θMax = model.uMin, model.uMax, model.θMax
 
+    # Control bounds on Γ
     if i == 1
       return uMin - U[u_dim,k]
     elseif i == 2
       return U[u_dim,k] - uMax
+
+    # Directional control constraint
     elseif i == 3
       return U[u_dim,k]*cos(θMax) - U[u_dim-1,k]
+
+    # Control bounds on u via Γ
     elseif i == 4
       return U[1,k] - U[u_dim,k]
     elseif i == 5
@@ -209,45 +218,35 @@ function control_linear_constraints(model::Quadrotor, X, U, Xp, Up, k, i)
     end
 end
 
-# Second order cone
-# function control_second_order_cone_constraints(model::Quadrotor, X, U, Xp, Up, k, i)
-#     x_dim, u_dim = model.x_dim, model.u_dim
-#     uMin, uMax, θMax = model.uMin, model.uMax, model.θMax
 
-#     if i == 1
-#       return U[u_dim,k], U[1:u_dim-1,k]
-#     else
-#       println("ERROR - TOO MANY SECOND ORDER CONE CONTROL CONSTRAINTS")
-#     end
-# end
 
-# Linearized second order cone
-# function control_second_order_cone_constraints(model::Quadrotor, X, U, Xp, Up, k, i)
-#     x_dim, u_dim = model.x_dim, model.u_dim
-#     uMin, uMax, θMax = model.uMin, model.uMax, model.θMax
-#     u, up, Γ, Γp = U[1:u_dim-1,k], Up[1:u_dim-1,k], U[u_dim,k], Up[u_dim,k]
-
-#     if i == 1
-#       return ( norm(up)^2 - Γp^2 + sum(2 * up[i] * (u[i] - up[i]) for i=1:u_dim-1) - 2 * Γp * (Γ - Γp) )
-#     else
-#       println("ERROR - TOO MANY SECOND ORDER CONE CONTROL CONSTRAINTS")
-#     end
-# end
+# State bounds and trust-region constraints (these are all convex constraints)
 
 function state_max_convex_constraints(model::Quadrotor, X, U, Xp, Up, k, i)
     return ( X[i, k] - model.xMax[i] )
 end
 
+
+
 function state_min_convex_constraints(model::Quadrotor, X, U, Xp, Up, k, i)
     return ( model.xMin[i] - X[i, k] )
 end
 
+
+
 function trust_region_max_constraints(model::Quadrotor, X, U, Xp, Up, k, i, Delta)
     return ( (X[i, k] - Xp[i, k]) - Delta )
 end
+
+
+
 function trust_region_min_constraints(model::Quadrotor, X, U, Xp, Up, k, i, Delta)
     return ( -Delta - (X[i, k] - Xp[i, k]) )
 end
+
+
+
+# Method that checks whether trust-region constraints are satisifed or not (recall that trust-region constraints are penalized)
 
 function is_in_trust_region(model::Quadrotor, X, U, Xp, Up, Delta)
     B_is_inside = true
@@ -266,13 +265,23 @@ function is_in_trust_region(model::Quadrotor, X, U, Xp, Up, Delta)
     return B_is_inside
 end
 
+
+# Initial and final conditions on state variables
+
 function state_initial_constraints(model::Quadrotor, X, U, Xp, Up)
     return (X[:,1] - model.x_init)
 end
 
+
+
 function state_final_constraints(model::Quadrotor, X, U, Xp, Up)
     return (X[:,end] - model.x_final)
 end
+
+
+
+# Methods that return the cylindrical obstacle-avoidance constraint and its lienarized version
+# Here, a merely classical distance function is considered
 
 function obstacle_constraint(model::Quadrotor, X, U, Xp, Up, k, obs_i)
     dimR  = Int(model.x_dim/2)
@@ -280,21 +289,18 @@ function obstacle_constraint(model::Quadrotor, X, U, Xp, Up, k, obs_i)
     p_k  = X[1:dimR-1,k]
     
     dist = norm(p_k - p_obs, 2)
-    # constraint = -( dist^2 - obs_radius^2 )
     constraint = -( dist - obs_radius )
 
     return constraint
 end
+
+
 
 function obstacle_constraint_convexified(model::Quadrotor, X, U, Xp, Up, k, obs_i)
     dimR  = Int(model.x_dim/2)
     p_obs, obs_radius = model.obstacles[obs_i][1], model.obstacles[obs_i][2]
     p_k  = X[1:dimR-1,k]
     p_kp = Xp[1:dimR-1,k]
-    
-    # dist_prev = norm(p_kp - p_obs, 2)
-    # dir_prev = p_kp - p_obs
-    # constraint = -( dist_prev^2 - obs_radius^2 + sum(2. * dir_prev[i] * (p_k[i] - p_kp[i]) for i=1:dimR-1) )
 
     dist_prev = norm(p_kp - p_obs, 2)
     dir_prev = (p_kp - p_obs)/dist_prev
@@ -303,15 +309,15 @@ function obstacle_constraint_convexified(model::Quadrotor, X, U, Xp, Up, k, obs_
     return constraint
 end
 
-# --------------------------------------------
+
+
+# The following methods return the dynamical constraints and their linearized versions
+# These are returned as time-discretized versions of the constraints " x' - f(x,u) = 0 " or " x' - A(t)*(x - xp) - B(t)*(u - up) = 0 ", respectively
 
 
 
-# --------------------------------------------
-# -                DYNAMICS                  -
-# --------------------------------------------
+# Method that returns the dynamical constraints and their linearized versions all at once
 
-# In continuous time, for all trajectory
 function compute_dynamics(model, Xp, Up)
     N = length(Xp[1,:])
 
@@ -331,6 +337,10 @@ function compute_dynamics(model, Xp, Up)
     return f_all, A_all, B_all
 end
 
+
+
+# These methods return the dynamics and its linearizations with respect to the state (matrix A(t)) and the control (matrix B(t)), respectively
+
 function f_dyn(x::Vector, u::Vector, model::Quadrotor)
   x_dim, u_dim = model.x_dim, model.u_dim
   dimR = Int(model.x_dim/2)
@@ -347,6 +357,8 @@ function f_dyn(x::Vector, u::Vector, model::Quadrotor)
   return f
 end
 
+
+
 function A_dyn(x::Vector, u::Vector, model::Quadrotor)
   x_dim, u_dim = model.x_dim, model.u_dim
   dimR = Int(model.x_dim/2)
@@ -359,6 +371,8 @@ function A_dyn(x::Vector, u::Vector, model::Quadrotor)
   return A
 end
 
+
+
 function B_dyn(x::Vector, u::Vector, model::Quadrotor)
   x_dim, u_dim = model.x_dim, model.u_dim
   dimR = Int(model.x_dim/2)
@@ -370,5 +384,3 @@ function B_dyn(x::Vector, u::Vector, model::Quadrotor)
 
   return B
 end
-
-# --------------------------------------------
